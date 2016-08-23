3bd80e509764f45883eeff89ae765feb398b2542
Anand Angalig and Nicolas Hurtado <anand@>
2016-08-23 14:59:54 -0400

    use an bject to sotre user information that used to determine the price that the customer will pay for a movie ticket

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
+function Customer(name) {
+  this.nameInput = name;
+  this.tickets = [];
+}
 
 function Ticket(age, time, movie) {
   this.ageInput = age;
   this.timeInput = time;
   this.movieInput = movie;
 }
+Ticket.prototype.ticketPrice = function () {
+  var price = 5;
 
-// Ticket.prototype.showMeTheMoney() {
-//   $("ul").text(price);
-// };
+  if (this.ageInput <= 60) {
+    price += 3;
+  }
+  if (this.timeInput === "full-price") {
+    price += 3;
+  }
+  if (this.movieInput === "new-release") {
+    price += 4;
+  }
+  return price;
+};
 
 $(document).ready(function() {
   $("form#userInput").submit(function (event) {
@@ -18,8 +32,33 @@ $(document).ready(function() {
     var inputtedMovie = $("#movie").val();
     var newTicket = new Ticket (inputtedAge, inputtedTime, inputtedMovie);
 
-    console.log(newTicket);
 
-
+    $("ul").append("<li>" + "Your ticket will cost you: " + "$"  + newTicket.ticketPrice() + "</li>");
   });
 });
+
+    // if (newTicket.ageInput >= 60 && newTicket.timeInput === "matinee" && newTicket.movieInput === "old-release") {
+    //   var price = 5;
+    //   console.log(price)
+    // } else if (newTicket.ageInput >= 60 && newTicket.timeInput === "matinee" && newTicket.movieInput === "new-release") {
+    //   var price = 9;
+    //   console.log(price);
+    // } else if (newTicket.ageInput >= 60 && newTicket.timeInput === "full-price" && newTicket.movieInput === "new-release") {
+    //   var price = 12;
+    //   console.log(price);
+    // }  else if (newTicket.ageInput >= 60 && newTicket.timeInput === "full-price" && newTicket.movieInput === "old-release") {
+    //   var price = 8;
+    //   console.log(price);
+    // } else if (newTicket.ageInput <= 60 && newTicket.timeInput === "matinee" && newTicket.movieInput === "new-release") {
+    //   var price = 12;
+    //   console.log(price);
+    // } else if (newTicket.ageInput <= 60 && newTicket.timeInput === "matinee" && newTicket.movieInput === "old-release") {
+    //   var price = 8;
+    //   console.log(price);
+    // } else if (newTicket.ageInput <= 60 && newTicket.timeInput === "full-price" && newTicket.movieInput === "new-release") {
+    //   var price = 15;
+    //   console.log(price);
+    // } else if (newTicket.ageInput <= 60 && newTicket.timeInput === "full-price" && newTicket.movieInput === "old-release") {
+    //   var price = 12;
+    //   console.log(price);
+    // }
